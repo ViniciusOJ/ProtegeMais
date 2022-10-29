e433a2916c77e540647b4ad3834a9c9f627c12c2
Vinicius Oliveira <contato.2002vinicius@gmail.com>
2022-10-29 11:31:27 -0300

    Atualização ANIMAÇOES

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal( { reset: true } );
+
+sr.reveal('.visible', { duration: 1100 })
+
+sr.reveal('.visible-rotate', { 
+    rotate: {x: 0, y: 80, z:0},
+    duration: 1600 
+})
